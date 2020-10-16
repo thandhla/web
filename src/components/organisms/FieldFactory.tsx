573bfd55a41325a1b09a1ddf5d7a4657660c19df
Moreblessing <cmoreb02@gmail.com>
2020-10-16 20:44:33 +0200

    number field

--- src/components/organisms/FieldFactory.tsx
+++ src/components/organisms/FieldFactory.tsx
@@ -3,10 +3,10 @@ import Caution from '../atoms/Caution/Caution';
 import LineField from '../molecules/LineField';
 import DropdownField from '../molecules/DropdownField';
 import RelationField from '../molecules/RelationField';
+import NumberField from '../molecules/NumberField';
 /*
 import EmailField from '../molecules/EmailField/EmailField';
 import LinkField from '../molecules/LinkField/LinkField';
-import NumberField from '../molecules/NumberField/NumberField';
 import DateField from '../molecules/DateField/DateField';
 import MultiSelectField from '../molecules/MultiSelectField/MultiSelectField';
 import TextBoxField from '../molecules/TextBoxField/TextBoxField';
@@ -20,11 +20,11 @@ const FieldFactory = (props: any) => {
       return <DropdownField { ...props } />;
     case 'relation':
       return <RelationField { ...props } />;
+    case 'number':
+      return <NumberField { ...props } />;
     /*
     case 'email':
       return <EmailField { ...props } />;
-    case 'number':
-      return <NumberField { ...props } />;
     case 'link':
       return <LinkField { ...props } />;
     case 'date':
